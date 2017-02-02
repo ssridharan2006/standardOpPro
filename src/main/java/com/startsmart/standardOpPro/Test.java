02d59b9324754fc875cd627453909f2a56dd1d2d
ssubramaniam2 <Sanjeev_Subramniam2@intuit.com>
2017-02-02 11:33:52 +0530

    added a test method

--- src/main/java/com/startsmart/standardOpPro/Test.java
+++ src/main/java/com/startsmart/standardOpPro/Test.java
@@ -2,12 +2,15 @@ package com.startsmart.standardOpPro;
 
 public class Test
 {
-  public static void main(String[] args)
-  {
-    System.out.println("SOP = Standard Operating Procedure 3");
-  }
-  public static void test()
-  {
-    System.out.println("SOP = Test 3");
-  }
+	public static void main(String[] args)
+	{
+		System.out.println("SOP = Standard Operating Procedure 1");
+		System.out.println("SOP = Standard Operating Procedure 2");
+		System.out.println("SOP = Standard Operating Procedure 3");
+	}
+
+	public static void test()
+	{
+		System.out.println("SOP = Test 3");
+	}
 }
