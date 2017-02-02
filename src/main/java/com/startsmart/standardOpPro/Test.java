c8670510506ef598f96926c0934b369bf426ecfc
sanjeevs1027 <sanjeev.cse1990@gmail.com>
2017-02-02 11:56:11 +0530

    Update Test.java
    remote commit for making conflict

--- src/main/java/com/startsmart/standardOpPro/Test.java
+++ src/main/java/com/startsmart/standardOpPro/Test.java
@@ -7,12 +7,13 @@ public class Test
 		System.out.println("SOP = Standard Operating Procedure 1");
 		System.out.println("SOP = Standard Operating Procedure 2");
 		System.out.println("SOP = Standard Operating Procedure 3");
-		System.out.println("SOP = Standard Operating Procedure 4");
+		System.out.println("SOP = Standard Operating Procedure 444");
 		System.out.println("SOP = Standard Operating Procedure 5");
 	}
 
 	public static void test()
 	{
 		System.out.println("SOP = Test 3");
+		System.out.println("SOP = Test 599");
 	}
 }
