7d7a5986e4b14625f925f216a3c8836c8d0f4a95
sanjeevs1027 <sanjeev.cse1990@gmail.com>
2017-02-03 18:14:07 +0530

    New project checkin

---
+++ project2/src/com/startsmart/standardOpPro/Solve.java
@@ -0,0 +1,11 @@
+package com.startsmart.standardOpPro;
+
+public class Solve
+{
+	public static void main(String[] args)
+	{
+		System.out.println("It seems you have a problem???");
+		System.out.println("No worries...");
+		System.out.println("I'm here to solve!!!");
+	}
+}
