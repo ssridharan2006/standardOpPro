188e90accf70b1724fc5da6521f0f790507cbafa
sanjeevs1027 <sanjeev.cse1990@gmail.com>
2017-02-08 14:02:30 +0530

    Remote branch change 3

--- project1/src/main/java/com/startsmart/standardOpPro/Test.java
+++ project1/src/main/java/com/startsmart/standardOpPro/Test.java
@@ -15,7 +15,9 @@ public class Test
 		System.out.println("commit 2 for merge test");
 		System.out.println("commit 3 for merge test");
 		System.out.println("Commit3 after pull request");
-
+		System.out.println("Remote branch change 1");
+		System.out.println("Remote branch change 2");
+		System.out.println("Remote branch change 3");
 	}
 
 	public static void test()
