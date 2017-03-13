9cadb235df8210936ebde926ac64c984dbaf68e6
sanjeevs1027 <sanjeev.cse1990@gmail.com>
2017-03-13 17:29:02 +0530

    CCSM3 commit 2

--- project1/src/main/java/com/startsmart/standardOpPro/Test.java
+++ project1/src/main/java/com/startsmart/standardOpPro/Test.java
@@ -12,6 +12,7 @@ public class Test
 		System.out.println("Pushing to workbranch1");
 		System.out.println("Pushing to workbranch5 and merging with master");
 		System.out.println("commit 1 for merge test");
+		System.out.println("commit !!!! for merge test");
 		System.out.println("commit 2 for merge test");
 		System.out.println("commit 3 for merge test");
 		System.out.println("Commit3 after pull request");
